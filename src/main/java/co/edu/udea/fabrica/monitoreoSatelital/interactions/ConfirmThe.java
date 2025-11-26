3e7bd6460b3481191c911d556f95b7f4d413cced
MrRichar02 <richarmamadi03@gmail.com>
2025-11-26 07:28:09 -0500

    commit

---
+++ src/main/java/co/edu/udea/fabrica/monitoreoSatelital/interactions/ConfirmThe.java
@@ -0,0 +1,14 @@
+package co.edu.udea.fabrica.monitoreoSatelital.interactions;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Interaction;
+
+public class ConfirmThe implements Interaction {
+
+	@Override
+	public <T extends Actor> void performAs(T actor) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'performAs'");
+	}
+
+}
